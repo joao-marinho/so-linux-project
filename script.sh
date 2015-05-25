25f478f6178f47431ed18bb1481c7f84e11759ef
JoaoB <joao.batista665@gmail.com>
2015-05-24 21:23:10 -0300

    Added arm-none-linux-gnueabi-i686 and hello.c

--- script.sh
+++ script.sh
@@ -6,6 +6,12 @@ LINUX_KERNEL_FILE=linux-2.6.39.tar.bz2
 LINUX_KERNEL_DIRECTORY=linux-2.6.39
 LINUX_KERNEL_URL=https://www.kernel.org/pub/linux/kernel/v2.6/linux-2.6.39.tar.bz2
 
+TOOLCHAIN_LINUX_FILE=arm-2013.05-24-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+TOOLCHAIN_LINUX_DIRECTORY=arm-2013.05
+TOOLCHAIN_LINUX_URL=https://dl.dropboxusercontent.com/u/43077685/arm-2013.05-24-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+
+
+
 DOWNLOAD_DIRECTORY=downloads
 
 PWD=$(pwd)
@@ -33,6 +39,23 @@ if [ ! -d $TOOLCHAIN_DIRECTORY ]; then
     
     tar -xjf $TOOLCHAIN_FILE
     mv $TOOLCHAIN_FILE $DOWNLOAD_DIRECTORY
+fi
+
+if [ ! -d $TOOLCHAIN_LINUX_DIRECTORY ]; then
+
+    if [ ! -f $TOOLCHAIN_LINUX_FILE ]; then
+        wget $TOOLCHAIN_LINUX_URL
+    fi
+    
+    tar -xjf $TOOLCHAIN_LINUX_FILE
+    mv $TOOLCHAIN_LINUX_FILE  $DOWNLOAD_DIRECTORY
+fi
+
+if grep -Fxq "export PATH=\$PATH:$PWD/$TOOLCHAIN_LINUX_DIRECTORY/bin" ~/.bashrc
+then
+    echo have
+else
+    echo "export PATH=\$PATH:$PWD/$TOOLCHAIN_LINUX_DIRECTORY/bin" >> ~/.bashrc
 fi
 
 if [ ! -d $LINUX_KERNEL_DIRECTORY ]; then
@@ -63,5 +86,23 @@ wget http://www.pcs.usp.br/~jkinoshi/2013/Makefile
 
 make ARCH=arm CROSS_COMPILE=arm-none-eabi- all
 
-cd arch/arm/boot
-qemu-system-arm -M versatilepb -m 128M -kernel zImage
+cd ..
+
+if [ ! -d linux-compiled ]; then
+    mkdir linux-compiled
+fi
+
+cp $LINUX_KERNEL_DIRECTORY/arch/arm/boot/zImage ./linux-compiled/
+
+touch hello.c
+echo "#include <stdio.h>" > hello.c
+echo "int main() {" >> hello.c
+echo "printf(\"Hello World!\\n\");" >> hello.c
+echo "while(1);" >> hello.c
+echo "return 0;" >> hello.c
+echo "}" >> hello.c
+
+arm-none-linux-gnueabi-gcc -static hello.c -o hello
+echo hello | cpio -o --format=newc > rootfs
+
+qemu-system-arm -M versatilepb -m 128M -kernel zImage -initrd rootfs -append "root=/dev/ram rdinit=/hello" -nographic
